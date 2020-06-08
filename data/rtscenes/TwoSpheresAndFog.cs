ac15ffef0929a00663d6ad7d566f92f5edcb1e43
Damian Wałoszek <DWaloszek@seznam.cz>
2020-06-09 01:05:27 +0200

    Created module with fog. Might be not working exactly right because of bugs in program.

---
+++ data/rtscenes/TwoSpheresAndFog.cs
@@ -0,0 +1,81 @@
+//////////////////////////////////////////////////
+///Externals.
+
+using DamianWaloszek;
+
+//////////////////////////////////////////////////
+// Rendering params.
+
+Debug.Assert(scene != null);
+Debug.Assert(context != null);
+
+// Set Ray Tracing algorith to the one that can handle fog.
+// If it's using normal RayTracing class, then it's because of a bug,
+// and you have to replace content of RayTracing.shade() function
+// with the content of RayTracingWithFog.shade().
+context[PropertyName.CTX_ALGORITHM] = new RayTracingWithFog();
+
+// Tooltip.
+context[PropertyName.CTX_TOOLTIP] = "r=<double> g=<double> b=<double> t=<double>\r(R, G, B, transparency of fog, all values in [0, 1])";
+
+// Params dictionary.
+Dictionary<string, string> p = Util.ParseKeyValueList(param);
+
+// Parsing params.
+double r = 0.5;
+Util.TryParse(p, "r", ref r);
+
+double g = 0.5;
+Util.TryParse(p, "g", ref g);
+
+double b = 0.5;
+Util.TryParse(p, "b", ref b);
+
+double t = 0.6;
+Util.TryParse(p, "t", ref t);
+
+//////////////////////////////////////////////////
+// CSG scene.
+
+CSGInnerNode root = new CSGInnerNode(SetOperation.Union);
+root.SetAttribute(PropertyName.REFLECTANCE_MODEL, new PhongModel());
+root.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] {1.0, 0.7, 0.1}, 0.1, 0.7, 0.3, 128, 1.0));
+scene.Intersectable = root;
+
+// Background color.
+scene.BackgroundColor = new double[] {0.0, 0.01, 0.03};
+scene.Background = new DefaultBackground(scene.BackgroundColor);
+
+// Camera.
+scene.Camera = new StaticCamera(new Vector3d(0.0, 0.5, -5.0),
+                                new Vector3d(0.0, -0.18, 1.0),
+                                70.0);
+
+// Light sources.
+scene.Sources = new System.Collections.Generic.LinkedList<ILightSource>();
+scene.Sources.Add(new AmbientLightSource(0.8));
+scene.Sources.Add(new PointLightSource(new Vector3d(-5.0, 4.0, -3.0), 1.2));
+
+// --- NODE DEFINITIONS ----------------------------------------------------
+
+// Fog Sphere.
+Fog fog = new Fog(new double[] { r, g, b }, t);
+Sphere s;
+s = new Sphere();
+s.SetAttribute(PropertyName.MATERIAL, fog);
+root.InsertChild(s, Matrix4d.Scale(2) * Matrix4d.CreateTranslation(0, 1, 3));
+
+// Sphere that is partially inside the fog. The part inside isn't rendered
+// due to the wrong implementation of Intersect funciton.
+s = new Sphere();
+root.InsertChild(s, Matrix4d.Scale(1.2) * Matrix4d.CreateTranslation(1, 0.5, 3));
+
+// Normal sphere behind the fog.
+s = new Sphere();
+root.InsertChild(s, Matrix4d.Scale(1.2) * Matrix4d.CreateTranslation(-2, 1.5, 8));
+
+// Infinite plane with checker.
+Plane pl = new Plane();
+pl.SetAttribute(PropertyName.COLOR, new double[] {0.3, 0.0, 0.0});
+pl.SetAttribute(PropertyName.TEXTURE, new CheckerTexture(0.6, 0.6, new double[] {1.0, 1.0, 1.0}));
+root.InsertChild(pl, Matrix4d.RotateX(-MathHelper.PiOver2) * Matrix4d.CreateTranslation(0.0, -1.0, 0.0));
---
+++ newmodules/DamianWaloszek-UniformFog/Fog.cs
@@ -0,0 +1,326 @@
+using MathSupport;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using Utilities;
+
+using Rendering;
+
+namespace DamianWaloszek
+{
+
+  /// <summary>
+  /// Ray-tracing rendering (w all secondary rays) with abaility to calculate fog.
+  /// </summary>
+  [Serializable]
+  public class RayTracingWithFog : RayTracing
+  {
+    /// <summary>
+    /// Recursive shading function - computes color contribution of the given ray (shot from the
+    /// origin 'p0' into direction vector 'p1''). Recursion is stopped
+    /// by a hybrid method: 'importance' and 'level' are checked.
+    /// Internal integration support.
+    /// </summary>
+    /// <param name="depth">Current recursion depth.</param>
+    /// <param name="importance">Importance of the current ray.</param>
+    /// <param name="p0">Ray origin.</param>
+    /// <param name="p1">Ray direction vector.</param>
+    /// <param name="color">Result color.</param>
+    /// <returns>Hash-value (ray sub-signature) used for adaptive subsampling.</returns>
+    protected override long shade (int depth,
+                                  double importance,
+                                  ref Vector3d p0,
+                                  ref Vector3d p1,
+                                  double[] color)
+    {
+      Vector3d direction = p1;
+
+      int bands = color.Length;
+      LinkedList<Intersection> intersections = scene.Intersectable.Intersect(p0, p1);
+
+      // If the ray is primary, increment both counters
+      Statistics.IncrementRaysCounters(1, depth == 0);
+
+      Intersection i = Intersection.FirstIntersection(intersections, ref p1);
+
+      if (i == null)
+      {
+        // No intersection -> background color
+        rayRegisterer?.RegisterRay(AbstractRayRegisterer.RayType.rayVisualizerNormal, depth, p0, direction * 100000);
+
+        return scene.Background.GetColor(p1, color);
+      }
+
+      // There was at least one intersection
+      i.Complete();
+
+      // calculating fog contribution if we are in fog
+      double[] fogColorContribution = null;
+      if (i.Material is Fog && intersections.Find(i).Next != null && intersections.Find(i).Next.Value != null)
+      {
+        Intersection j = intersections.Find(i).Next.Value;
+        j.Complete();
+
+        fogColorContribution = new double[4]; //RGB + Alpha
+        fogColorContribution[0] = i.Material.Color[0];
+        fogColorContribution[1] = i.Material.Color[1];
+        fogColorContribution[2] = i.Material.Color[2];
+
+        Vector3d start = i.CoordWorld;
+        Vector3d end = j.CoordWorld;
+        double distance = Vector3d.Distance(start, end);
+        double alphaOfFog = 1 - Math.Pow(i.Material.Kt, distance);
+        fogColorContribution[3] = alphaOfFog;
+
+        if (j.Material is Fog)
+        {
+          // i is the start of fog, j the end, so take another intersection
+          if (intersections.Find(j).Next != null && intersections.Find(j).Next.Value != null)
+          {
+            i = intersections.Find(j).Next.Value;
+            i.Complete();
+          }
+          else
+          {
+            // No intersection except the fog -> background color + fog
+            rayRegisterer?.RegisterRay(AbstractRayRegisterer.RayType.rayVisualizerNormal, depth, p0, direction * 100000);
+
+            long res = scene.Background.GetColor(p1, color);
+
+            for (int k = 0; k < 3; ++k)
+            {
+              color[k] = color[k] * (1 - fogColorContribution[3]) + fogColorContribution[k] * fogColorContribution[3];
+            }
+
+            return res;
+          }
+
+        }
+        else
+        {
+          i = j;
+        }
+      }
+
+
+      rayRegisterer?.RegisterRay(AbstractRayRegisterer.RayType.unknown, depth, p0, i);
+
+      // Hash code for adaptive supersampling
+      long hash = i.Solid.GetHashCode();
+
+      // Apply all the textures first
+      if (i.Textures != null)
+        foreach (ITexture tex in i.Textures)
+          hash = hash * HASH_TEXTURE + tex.Apply(i);
+
+      if (MT.pointCloudCheckBox && !MT.pointCloudSavingInProgress && !MT.singleRayTracing)
+      {
+        foreach (Intersection intersection in intersections)
+        {
+          if (!intersection.completed)
+            intersection.Complete();
+
+          if (intersection.Textures != null && !intersection.textureApplied)
+            foreach (ITexture tex in intersection.Textures)
+              tex.Apply(intersection);
+
+          double[] vertexColor = new double[3];
+          Util.ColorCopy(intersection.SurfaceColor, vertexColor);
+          Master.singleton?.pointCloud?.AddToPointCloud(intersection.CoordWorld, vertexColor, intersection.Normal, MT.threadID);
+        }
+      }
+
+      // Color accumulation.
+      Array.Clear(color, 0, bands);
+      double[] comp = new double[bands];
+
+      // Optional override ray-processing (procedural).
+      if (DoRecursion &&
+          i.Solid?.GetAttribute(PropertyName.RECURSION) is RecursionFunction rf)
+      {
+        hash += HASH_RECURSION * rf(i, p1, importance, out RayRecursion rr);
+
+        if (rr != null)
+        {
+          // Direct contribution.
+          if (rr.DirectContribution != null &&
+              rr.DirectContribution.Length > 0)
+            if (rr.DirectContribution.Length == 1)
+              Util.ColorAdd(rr.DirectContribution[0], color);
+            else
+              Util.ColorAdd(rr.DirectContribution, color);
+
+          // Recursive rays.
+          if (rr.Rays != null &&
+              depth++ < MaxLevel)
+            foreach (var ray in rr.Rays)
+            {
+              RayRecursion.RayContribution rc = ray;
+              hash += HASH_REFLECT * shade(depth, rc.importance, ref rc.origin, ref rc.direction, comp);
+
+              // Combine colors.
+              if (ray.coefficient == null)
+                Util.ColorAdd(comp, color);
+              else
+              if (ray.coefficient.Length == 1)
+                Util.ColorAdd(comp, ray.coefficient[0], color);
+              else
+                Util.ColorAdd(comp, ray.coefficient, color);
+            }
+
+          return hash;
+        }
+      }
+
+      // Default (Whitted) ray-tracing interaction (lights [+ reflection] [+ refraction]).
+      p1 = -p1; // viewing vector
+      p1.Normalize();
+
+      if (scene.Sources == null || scene.Sources.Count < 1)
+        // No light sources at all.
+        Util.ColorAdd(i.SurfaceColor, color);
+      else
+      {
+        // Apply the reflectance model for each source.
+        i.Material = (IMaterial)i.Material.Clone();
+        i.Material.Color = i.SurfaceColor;
+
+        foreach (ILightSource source in scene.Sources)
+        {
+          double[] intensity = source.GetIntensity(i, out Vector3d dir);
+
+          if (MT.singleRayTracing && source.position != null)
+            // Register shadow ray for RayVisualizer.
+            rayRegisterer?.RegisterRay(AbstractRayRegisterer.RayType.rayVisualizerShadow, i.CoordWorld, (Vector3d)source.position);
+
+          if (intensity != null)
+          {
+            if (DoShadows && dir != Vector3d.Zero)
+            {
+              intersections = scene.Intersectable.Intersect(i.CoordWorld, dir);
+              Statistics.allRaysCount++;
+              Intersection si = Intersection.FirstRealIntersection(intersections, ref dir);
+              // Better shadow testing: intersection between 0.0 and 1.0 kills the lighting.
+              if (si != null && !si.Far(1.0, ref dir))
+                continue;
+            }
+
+            double[] reflection = i.ReflectanceModel.ColorReflection(i, dir, p1, ReflectionComponent.ALL);
+            if (reflection != null)
+            {
+              Util.ColorAdd(intensity, reflection, color);
+              hash = hash * HASH_LIGHT + source.GetHashCode();
+            }
+          }
+        }
+      }
+
+      // if we were in fog, mix with appropriate color
+      /*if (fogColorContribution != null)
+      {
+        for (int k = 0; k < 3; ++k)
+        {
+          color[k] = color[k] * (1 - fogColorContribution[3]) + fogColorContribution[k] * fogColorContribution[3];
+        }
+      }*/
+
+      // Check the recursion depth.
+      if (depth++ >= MaxLevel || (!DoReflections && !DoRefractions) || i.Material is Fog)
+        // No further recursion.
+        return hash;
+
+      Vector3d r;
+      double   maxK;
+      double   newImportance;
+
+      if (DoReflections)
+      {
+        // Shooting a reflected ray.
+        Geometry.SpecularReflection(ref i.Normal, ref p1, out r);
+        double[] ks = i.ReflectanceModel.ColorReflection(i, p1, r, ReflectionComponent.SPECULAR_REFLECTION);
+        if (ks != null)
+        {
+          maxK = ks[0];
+          for (int b = 1; b < bands; b++)
+            if (ks[b] > maxK)
+              maxK = ks[b];
+
+          newImportance = importance * maxK;
+          if (newImportance >= MinImportance)
+          {
+            // Do compute the reflected ray.
+            hash += HASH_REFLECT * shade(depth, newImportance, ref i.CoordWorld, ref r, comp);
+            Util.ColorAdd(comp, ks, color);
+          }
+        }
+      }
+
+      if (DoRefractions)
+      {
+        // Shooting a refracted ray.
+        maxK = i.Material.Kt;   // simple solution - no influence of reflectance model yet
+        newImportance = importance * maxK;
+        if (newImportance < MinImportance)
+          return hash;
+
+        // Refracted ray.
+        if ((r = Geometry.SpecularRefraction(i.Normal, i.Material.n, p1)) == Vector3d.Zero)
+          return hash;
+
+        hash += HASH_REFRACT * shade(depth, newImportance, ref i.CoordWorld, ref r, comp);
+        Util.ColorAdd(comp, maxK, color);
+      }
+
+      return hash;
+    }
+  }
+
+
+  /// <summary>
+  /// Simple Phong-like reflectance model: material description.
+  /// </summary>
+  [Serializable]
+  public class Fog : IMaterial
+  {
+    /// <summary>
+    /// Base surface color.
+    /// </summary>
+    public double[] Color { get; set; }
+
+    /// <summary>
+    /// Coefficient of transparency.
+    /// </summary>
+    public double Kt { get; set; }
+
+    /// <summary>
+    /// Absolute index of refraction.
+    /// </summary>
+    public double n
+    {
+      get => 0;
+      set { }
+    }
+
+    public Fog(double[] color, double transparency)
+    {
+      Color = color;
+      Kt = transparency;
+    }
+
+    public Fog (Fog f)
+    {
+      Color = (double[])f.Color.Clone();
+      Kt = f.Kt;
+    }
+
+    public Fog() : this(new double[] { 0.5, 0.5, 0.5 }, 0.6)
+    { }
+    
+
+    public object Clone ()
+    {
+      return new Fog(this);
+    }
+  }
+
+}
